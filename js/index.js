489d549692d4f3384c0a2ad0839b03e1e3bf62fc
Mauricio Rodriguez <maurijrp2001@gmail.com>
2020-06-19 15:09:27 -0500

    background color change, team section improved

--- js/index.js
+++ js/index.js
@@ -1,12 +1,22 @@
-let button = document.getElementById('button');
-const menu = document.getElementById('menu')
-console.log(menu)
-button.addEventListener('click', hamburgerMenu);
+let mediaQuerie = window.matchMedia('screen and (max-width: 500px)');
+let button = document.getElementById('menu-button');
+const menu = document.getElementById('menu');
+
+changeScreenSize(mediaQuerie);
+mediaQuerie.addListener(changeScreenSize);
+
+function changeScreenSize(changeQuerie) {
+    if (changeQuerie.matches) {
+        button.addEventListener('click', hamburgerMenu);
+    } else {
+        button.removeEventListener('click', hamburgerMenu);
+    }
+}
 
 function hamburgerMenu() {
     if ( menu.className == 'header__ul') {
-        menu.className += ' header__ul-active'
+        menu.className += ' header__ul-active';
     } else {
-        menu.className = 'header__ul'
+        menu.className = 'header__ul';
     }
 };
